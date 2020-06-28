a5c45c801a930c23a1cfebf3dfd8391b090c0938
duong755 <ngo.quang.duong.1100@gmail.com>
2020-06-28 19:43:31 +0700

    Move to starter directory

--- inventory/default.py
+++
@@ -1,9 +0,0 @@
-"""Inventory file"""
-import boto3
-
-ec2 = boto3.resource('ec2')
-
-instance = ec2.Instance('i-04827f4c58e62a4e0')
-instance2 = ec2.Instance('i-087bc4f5e2d3ef2a1')
-
-print(instance.public_ip_address, instance2.public_ip_address)
